4abe20f170a5a61512743123ebab0d27def798f7
GayathriAnandhan <gayathrianandhan2812@gmail.com>
2026-04-18 15:54:37 +0530

    Alignment changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 Gayathri Anandhan | All Rights Reserved</p>
+      <p>© 2026 Gayathri Anandhan | Java Full Stack Developer</p>
     </footer>
   );
 }
