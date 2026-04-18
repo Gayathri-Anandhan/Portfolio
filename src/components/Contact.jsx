c1c715721eff195f44366948dda5f52c07e1c0a6
GayathriAnandhan <gayathrianandhan2812@gmail.com>
2026-04-18 16:25:54 +0530

    Contact section corrections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,36 @@
 import '../styles/contact.css';
+
 function Contact() {
     return (
         <section id="contact">
-            <h2 id="Contact">CONTACT</h2>
-            <p>EMAIL ID: gayathrianandhan2812@gmail.com</p>
-            <p>GITHUB: https://github.com/Gayathri-Anandhan</p>
-            <p>LINKEDIN: https://www.linkedin.com/in/gayathri-a-09267a225/</p>
-        </section >
-    )
+            <h2>Contact Me</h2>
+
+            <div className="contact-card">
+
+                <p>
+                    <span>Email:</span>
+                    <a href="mailto:gayathrianandhan2812@gmail.com">
+                        gayathrianandhan2812@gmail.com
+                    </a>
+                </p>
+
+                <p>
+                    <span>GitHub:</span>
+                    <a href="https://github.com/Gayathri-Anandhan" target="_blank">
+                        github.com/Gayathri-Anandhan
+                    </a>
+                </p>
+
+                <p>
+                    <span>LinkedIn:</span>
+                    <a href="https://www.linkedin.com/in/gayathri-a-09267a225/" target="_blank">
+                        linkedin.com/in/gayathri-a
+                    </a>
+                </p>
+
+            </div>
+        </section>
+    );
 }
+
 export default Contact;
